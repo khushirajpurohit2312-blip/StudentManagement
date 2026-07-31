89ce86ca4b32c7d5704111e647c61bcd837ad5ae
Khushi Rajpurohit <khushirajpurohit2312@gmail.com>
2026-07-31 19:30:27 +0530

    initial project setup

---
+++ script.js
@@ -0,0 +1,81 @@
+let students=[];
+
+function addStudent(){
+
+let id=document.getElementById("id").value;
+
+let name=document.getElementById("name").value;
+
+let course=document.getElementById("course").value;
+
+students.push({
+
+id,
+
+name,
+
+course
+
+});
+
+alert("Student Added");
+
+}
+
+function displayStudents(){
+
+let data="";
+
+students.forEach(student=>{
+
+data+=`
+
+<p>
+
+${student.id}
+
+${student.name}
+
+${student.course}
+
+</p>
+
+`;
+
+});
+
+document.getElementById("output").innerHTML=data;
+
+}
+
+function searchStudent(){
+
+let id=document.getElementById("id").value;
+
+let student=students.find(s=>s.id===id);
+
+if(student){
+
+document.getElementById("output").innerHTML=
+
+student.id+" "+student.name+" "+student.course;
+
+}
+
+else{
+
+alert("Student Not Found");
+
+}
+
+}
+
+function deleteStudent(){
+
+let id=document.getElementById("id").value;
+
+students=students.filter(s=>s.id!==id);
+
+alert("Deleted");
+
+}
